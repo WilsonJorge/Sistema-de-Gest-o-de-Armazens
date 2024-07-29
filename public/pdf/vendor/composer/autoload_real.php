26832f5c666e4f10963fa0e29cd0d04244cd5d5d
Adelson Saguate <adelsonsaguate@acsun-mz.com>
2024-07-29 08:48:47 +0200

    Config de alguns ficheiros

--- public/pdf/vendor/composer/autoload_real.php
+++ public/pdf/vendor/composer/autoload_real.php
@@ -29,29 +29,22 @@ class ComposerAutoloaderInitad4ed7d3c569de5ea0c098c860e84c79
         spl_autoload_unregister(array('ComposerAutoloaderInitad4ed7d3c569de5ea0c098c860e84c79', 'loadClassLoader'));
 
         require __DIR__ . '/autoload_static.php';
-        \Composer\Autoload\ComposerStaticInitad4ed7d3c569de5ea0c098c860e84c79::getInitializer($loader)();
+        call_user_func(\Composer\Autoload\ComposerStaticInitad4ed7d3c569de5ea0c098c860e84c79::getInitializer($loader));
 
         $loader->register(true);
 
-        $includeFiles = \Composer\Autoload\ComposerStaticInitad4ed7d3c569de5ea0c098c860e84c79::$files;
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequiread4ed7d3c569de5ea0c098c860e84c79($fileIdentifier, $file);
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitad4ed7d3c569de5ea0c098c860e84c79::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
-    }
-}
-
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
-function composerRequiread4ed7d3c569de5ea0c098c860e84c79($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-        require $file;
     }
 }
